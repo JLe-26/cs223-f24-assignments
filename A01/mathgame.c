3c46cdaaaf8d8b93e19bcdc8f388e496b86a2548
Jenny Le <you@example.com>
2024-09-06 15:21:26 -0400

    First HW

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,11 +1,39 @@
 /***************************************************
  * mathgame.c
- * Author: 
+ * Author: Jenny Le
  * Implements a math game
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
+ int r; //num rounds
+ int i = 0; //iterator
+ int counter = 0; //score
+ printf("Welcome to Math Game!\nHow many rounds do you want to play?\t"); 
+ scanf("%d", &r); //reads player response
+
+ //game loop
+ for(i;i<r;i++){
+ srand(time(NULL)); //seeds the random number generator according to time 
+ int val1 = (rand() % 10) + 1; //randomized values
+ int val2 = (rand() % 10) + 1;
+ int answer; //player response
+
+ printf("%i %s %i %s\t %s\t", val1, "+", val2, "=", "?"); //prints math question
+ scanf("%i", &answer);
+ if(answer == val1 + val2){
+	 printf("Correct!\n");
+	 counter++;
+ }
+ else{
+	 printf("Incorrect :(\n");
+ }
+ }
+ printf("%s %i %s %i %s\n", "You answered", counter, "/", r, "correctly."); 
   return 0;
 }
+
+
