43daf0f7268b9ed446d74e149b9d0a9444d4e6dc
Jenny Le <you@example.com>
2024-09-21 23:35:52 -0400

    assignment 3 complete

--- A03/dynamic_songs.c
+++ A03/dynamic_songs.c
@@ -1,10 +1,68 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Jenny Le
+ * Date: September 21, 2024
+ * Description: This program reads songs from 
+ * a text file into an array and is able to set the 
+ * contents of the dynamic array of songs.
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void printSongList(struct Song array[]){
+  for (int i = 0; i < numSongs; i++) //Prints struct Song information
+  {
+	  printf("%d %c %-20s artist: %-20s duration: %d %c %d D: %.2f E: %f T: %f V: %f \n", i, ')', array[i].title, array[i].artist, array[i].duration/60000, ':', array[i].duration%60000, array[i].danceability, array[i].energy, array[i].tempo, array[i].valence);
+  }
+
+}
 
 int main() {
+  printf("Welcome to Jenny's Danceability Directory");
+
+  // Song struct
+  struct Song
+  {
+	  char *title[128];
+	  char *artist[128];
+	  int duration; // in milliseconds
+	  float danceability;
+	  float energy;
+	  float tempo;
+	  float valence;
+
+  };
+
+  struct Song song;
+
+  // Opens the songlist.csv file
+  FILE *infile;
+
+  infile = fopen("songlist.csv", "r");
+  if (infile == NULL)
+  {
+	  printf("Error: unable to open file %s\n", "songlist.csv");
+	  exit(1);
+  }
+
+  int numSongs;
+  struct Song array[] = malloc(sizeof(Song) * numSongs); 
+  
+  // Reads the file
+  char chunk[128];
+  while (fgets(chunk, sizeof(chunk), infile) != NULL)
+  {
+	  char *token = strtok(chunk, ',');
+          if (token != NULL)
+	  {
+		  numSongs = atoi(token);
+	  }
+
+  }
+
+
+  fclose(infile);
+  free(array);
   return 0;
 }
+
--- A03/slow.c
+++ A03/slow.c
@@ -1,10 +1,40 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Jenny Le 
+ * Date: September 20, 2024
+ * Description: This program turns a phrase into ent-speech
+ * by asking for pause length and containing '.' between each
+ * letter in the input.
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
+  int num;
+  char phrase[32];
+  char *slow_text;
+  int j = 0; // Iterator
+
+  printf("Pause length: \n ");
+  scanf("%d", &num);
+
+  printf("Text: \n");
+  scanf("%s", phrase);
+
+  // Allocates an array of chars with enough space for pauses
+  slow_text = malloc(sizeof(char) * (strlen(phrase)+((strlen(phrase)-1) * num)));
+
+  for(int i = 0; i < strlen(phrase); i++)
+  {
+	  if(phrase[i] != '\0'){
+		  slow_text[j++] = phrase[i];
+	  }
+	  for(int k = 0; k < num; k++)
+	  {
+		  slow_text[j++] = '.';
+	  } 
+  }  
+  printf("%s \n", slow_text);
+  free(slow_text);  
   return 0;
 }
